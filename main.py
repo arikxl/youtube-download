99ba8cdee560a44bf5ac28882c14ce25e3af8e73
Arik Alexandrov <arikxl@gmail.com>
2023-04-23 23:06:38 +0300

    finish

--- main.py
+++ main.py
@@ -2,6 +2,20 @@ import tkinter
 import customtkinter
 from pytube import YouTube
 
+def startDownload():
+    try:
+        ytLink = link.get()
+        ytObject = YouTube(ytLink)
+        video = ytObject.streams.get_highest_resolution()
+        # ytObject.streams.get_audio_only()
+        title.configure(text=ytObject.title, text_color="blue")
+        finishLabel.configure(text="")
+        video.download()
+        finishLabel.configure(text="Download complete!")
+    except:
+        finishLabel.configure(text="Youtube link is invalid", text_color="red")
+
+
 customtkinter.set_appearance_mode('System')
 customtkinter.set_default_color_theme('blue')
 
@@ -16,7 +30,11 @@ url_var= tkinter.StringVar()
 link = customtkinter.CTkEntry(app, width=350, height= 40, textvariable=url_var)
 link.pack()
 
+download = customtkinter.CTkButton(app, text="Download", command=startDownload)
+download.pack(padx=10, pady=10)
 
+finishLabel = customtkinter.CTkLabel(app, text="")
+finishLabel.pack()
 
 app.mainloop()
 
